d2bcd2d0b46573f3392ee554adaa65bb2a4becdc
BYTECODENINJA <josephmulwa808@outlook.com>
2026-07-30 09:36:12 +0300

    Complete API-GATEWAY service

---
+++ apps/api-gateway/src/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import { HttpService} from "@nestjs/axios";
+import { firstValueFrom } from 'rxjs';
+import { SERVICES_PORTS} from "@app/common";
+
+@Injectable()
+export class AuthService {
+    private readonly authUrl = `http://localhost:${SERVICES_PORTS.AUTH}`;
+
+    constructor(private readonly httpService: HttpService) {}
+
+    async register(data: { email: string; password: string; name: string; }) {
+        try {
+            const response = await firstValueFrom(
+                this.httpService.post(`${this.authUrl}/register`, data)
+            );
+            return response.data;
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
+
+    async login(data: {email: string, password: string}) {
+        try {
+            const response = await firstValueFrom(
+                this.httpService.post(`${this.authUrl}/login`, data)
+            );
+            return response.data;
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
+
+    async getProfile( token: string) {
+        try {
+            const response = await firstValueFrom(
+                this.httpService.get(`${this.authUrl}/profile`, {
+                    headers: { Authorization: token}
+                })
+            )
+
+            return response.data;
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
+
+    private handleError(error: any): never {
+        if (error.response) {
+            throw new HttpException(error.response.data, error.response.status);
+        }
+        throw new HttpException('Something went wrong', 503)
+    }
+}
